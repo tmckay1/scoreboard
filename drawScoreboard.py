147663c77909edbb65b833d32366dff2cabd1fad
Tyler McKay <tmckay1.jackson@gmail.com>
2020-07-21 21:30:57 -0400

    Fix single display

--- drawScoreboard.py
+++ drawScoreboard.py
@@ -4,10 +4,13 @@ from bibliopixel.drivers.PiWS281X import *
 import sys
 
 #create biblio pixel driver and led
-thread     = False   # display updates to run in background thread
-brightness = 100     # brightness 0-255
-driver     = PiWS281X(18*5*2 + 17*5)
-led        = Strip(driver, thread, brightness)
+thread             = False   # display updates to run in background thread
+brightness         = 100     # brightness 0-255
+numTeamNameNumLeds = 18*5    # number of leds in the team name
+numTimerLeds       = 17*5    # number of leds in the timer section
+numScoreLeds       = 3*5     # number of leds in for a single number
+driver             = PiWS281X(numTeamNameNumLeds*2 + numTimerLeds + numScoreLeds*4)
+led                = Strip(driver, thread, brightness)
 
 #run animation
 anim  = ScoreBoardAnimation(led, 1000, "message")
